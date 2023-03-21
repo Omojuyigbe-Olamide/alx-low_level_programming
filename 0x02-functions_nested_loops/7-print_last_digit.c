0cc95f50d519db4b48062bdcaf95f0dbdffbebd9
Omojuyigbe-Olamide <omojuyigbeolamide2@gmail.com>
2023-03-21 06:23:44 -0700

    8

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,4 +1,15 @@
 #include "main.h"
+
+/**
+ *print_last_digit - prints the last of a number
+ *
+ *@r: stores the last digit and goes through the condtional statements
+ *
+ *main - evaluates last digit
+ *
+ *Return: the value of the last digit
+ */
+
 int print_last_digit(int r)
 
 {
